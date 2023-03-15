e5f00f01fef73509948170c73f83d330eb51c319
Ervin Piol <112147440+ervinpiol@users.noreply.github.com>
2023-03-15 10:54:54 +0800

    Update NavLink.jsx

--- my-project/src/components/Reusable/NavLink.jsx
+++ my-project/src/components/Reusable/NavLink.jsx
@@ -4,10 +4,6 @@ import { Link, useLocation } from "react-router-dom";
 const NavLink = ({ to, text }) => {
   const location = useLocation();
 
-  /*   const isActive = (path) => {
-    return location.pathname === path ? "text-desaturatedcyan" : "";
-  }; */
-
   return (
     <Link to={to}>
       <li>{text}</li>
