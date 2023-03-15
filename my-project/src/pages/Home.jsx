38c8d9ddeddbdd958b37015e4a593b079b5f702b
Ervin Piol <112147440+ervinpiol@users.noreply.github.com>
2023-03-15 10:53:50 +0800

    Update Home.jsx

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,35 +1,3 @@
-/* import React from "react";
-import FrontPage from "../components/HomePage/FrontPage";
-import BGHomeDesktop from "../assets/home/background-home-desktop.jpg";
-import BGHomeTablet from "../assets/home/background-home-tablet.jpg";
-import BGImage from "../components/Reusable/BGImage";
-
-const images = [
-  {
-    src: BGHomeDesktop,
-    breakpoint: 1200,
-  },
-  {
-    src: BGHomeTablet,
-    breakpoint: 768,
-  },
-];
-
-const Home = () => {
-  return (
-    <div
-      className="flex h-screen bg-cover bg-center items-center"
-      style={{ backgroundImage: `url(${BGHomeDesktop})` }}
-    >
-      <div className="flex w-[1440px] md:w-auto ss:w-auto mx-auto flex-col ss:mt-24">
-        <FrontPage />
-      </div>
-    </div>
-  );
-};
-
-export default Home; */
-
 import React from "react";
 import FrontPage from "../components/HomePage/FrontPage";
 import BGHomeDesktop from "../assets/home/background-home-desktop.jpg";
